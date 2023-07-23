98de10c7a5a01f06e3962dd18c2e488ef6135ef7
balakethelock <111737968+balakethelock@users.noreply.github.com>
2023-07-23 22:26:22 +0100

    Update AutoMasterLooter.lua

--- AutoMasterLooter.lua
+++ AutoMasterLooter.lua
@@ -1,7 +1,7 @@
 -- Events
 AutoMasterLooter = CreateFrame("Frame","AutoMasterLooter",UIParent)
-AutoMasterLooter:RegisterEvent("PLAYER_ENTERING_WORLD");
-AutoMasterLooter:RegisterEvent("LOOT_OPENED");
+AutoMasterLooter:RegisterEvent("PLAYER_ENTERING_WORLD")
+AutoMasterLooter:RegisterEvent("LOOT_OPENED")
 AutoMasterLooter:SetScript("OnEvent", function() OpenLoot_OnEvent(event, arg1) end)
 
 -- Rares and epics that are subject to autoloot. ["exact ingame name of the item"] = "nickname (does nothing)"
@@ -10,6 +10,7 @@ LootedItemsTable["Wartorn Plate Scrap"] = "Plate scrap"
 LootedItemsTable["Wartorn Chain Scrap"] = "Mail Scrap"
 LootedItemsTable["Wartorn Leather Scrap"] = "leather Scrap"
 LootedItemsTable["Wartorn Cloth Scrap"] = "Cloth Scrap"
+LootedItemsTable["Frozen Rune"] = "Frozen Rune"
 LootedItemsTable["Idol of Death"] = "Idol of Death"
 LootedItemsTable["Idol of Life"] = "Idol of Life"
 LootedItemsTable["Idol of Night"] = "Idol of Night"
@@ -19,6 +20,17 @@ LootedItemsTable["Idol of War"] = "Idol of War "
 LootedItemsTable["Idol of the Sage"] = "Idol of the Sage "
 LootedItemsTable["Idol of the Sun"] = "Idol of the Sun"
 LootedItemsTable["Elementium Ore"] = "Elementium Ore"
+LootedItemsTable["Fiery Core"] = "Fiery Core"
+LootedItemsTable["Lava Core"] = "Lava Core"
+
+-- Whites and greens that are excluded from autoloot.
+ExcludedItemsTable = {}
+ExcludedItemsTable["Tome of Tranquilizing Shot"] = "Tranq Shot book"
+ExcludedItemsTable["Hazza'rah's Dream Thread"] = "Hazzarah"
+ExcludedItemsTable["Gri'lek's Blood"] = "Grilek"
+ExcludedItemsTable["Wushoolay's Mane"] = "Wushoolay"
+ExcludedItemsTable["Renataki's Tooth"] = "Renataki"
+ExcludedItemsTable["Mr. Bigglesworth"] = "KT cat"
 
 local AutoMasterLooter = 0
 
@@ -32,35 +44,44 @@ function AutoMasterLooterSwitch(cmd)
 	end
 end
 
-SLASH_AUTOMASTERLOOTER1 = '/AutoMasterLooter'
-SLASH_AUTOMASTERLOOTER2 = '/automasterlooter'
-SLASH_AUTOMASTERLOOTER3 = '/automl'
-SLASH_AUTOMASTERLOOTER4 = '/autoML'
-SLASH_AUTOMASTERLOOTER5 = '/AutoML'
-SLASH_AUTOMASTERLOOTER5 = '/Automl'
-SLASH_AUTOMASTERLOOTER6 = '/AutoMasterlooter'
+SLASH_AUTOMASTERLOOTER1 = '/automasterlooter'
+SLASH_AUTOMASTERLOOTER2 = '/automl'
+SLASH_AUTOMASTERLOOTER3 = '/automasterloot'
 SlashCmdList.AUTOMASTERLOOTER = AutoMasterLooterSwitch
 
 function OpenLoot_OnEvent()
-	lootmethod, masterlooterID = GetLootMethod() -- Only run if the player is the masterlooter.
-	if masterlooterID == 0 and AutoMasterLooter == 1 then
+	lootmethod, masterlooterID = GetLootMethod()
+	if masterlooterID == 0 and AutoMasterLooter == 1 then -- Only run if the player is the masterlooter.
 		local announcestring = "Epic inside! " -- Generate announce message
 		for looterindex = 1, 40 do
 			if (GetMasterLootCandidate(looterindex) == UnitName("player")) then
 				for lootedindex = 1, GetNumLootItems() do
 					lootIcon, lootName, lootQuantity, rarity = GetLootSlotInfo(lootedindex)
-					for LootTableName,LootTableAnnounce in pairs(LootedItemsTable) do
-						if LootTableName == lootName or rarity <3 then -- loot item if it's gray/white/green/listed blue
-							GiveMasterLoot(lootedindex, looterindex)
+					if rarity == 4 and lootName ~= "Elementium Ore" then
+						announcestring = announcestring..lootName.." ! " -- Add any found epics to the announce message. Except Elementium Ore, because it's automatically looted to the player.
+					end 
+					if rarity < 3 then
+						local IsExcludedItem = 0
+						for ExcludedTableName,ExcludedTableAnnounce in pairs(ExcludedItemsTable) do			
+							if ExcludedTableName == lootName then
+								IsExcludedItem = 1 -- Found a white/green item that shouldn't be masterlooted
+							elseif next(ExcludedItemsTable,ExcludedTableName) == nil and IsExcludedItem == 0 then
+								GiveMasterLoot(lootedindex, looterindex) -- If we went through all the items that should be excluded and none of them matched, loot the item.
+							end
+						end
+					else
+						for LootTableName,LootTableAnnounce in pairs(LootedItemsTable) do
+							if LootTableName == lootName or rarity <3 then
+								GiveMasterLoot(lootedindex, looterindex) -- loot item if it's gray/white/green/listed blue
+							end
 						end
 					end
-					if rarity == 4 and lootName ~= "Elementium Ore" then announcestring = announcestring..lootName.." ! " end -- Add any found epics to the announce message. Except Elementium Ore, because it's automatically looted to the player.
 				end
 			end
 		end
-		if announcestring ~= "Epic inside! " then -- Announce the message if any epics were added into it.
-			DEFAULT_CHAT_FRAME:AddMessage(announcestring)
-			PlaySound("AuctionWindowClose"); -- Play a warning sound. I choose auction house close, you can choose any sound you want from https://wowwiki-archive.fandom.com/wiki/API_PlaySound?oldid=313344 
+		if announcestring ~= "Epic inside! " then
+			DEFAULT_CHAT_FRAME:AddMessage(announcestring) -- Announce the message if any epics were added into it.
+			PlaySound("AuctionWindowClose") -- Play a warning sound. I chose auction house close, you can choose any sound you want from https://wowwiki-archive.fandom.com/wiki/API_PlaySound?oldid=313344 
 		end
 	end
 end
